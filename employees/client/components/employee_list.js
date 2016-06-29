cd3677376d66a9402971cc1edfe966b784b47f99
camperliz <analizapandac@gmail.com>
2016-06-29 09:11:11 +0800

    feature: populate employee directory with data coming from faker, added subscriptions and publications

--- employees/client/components/employee_list.js
+++ employees/client/components/employee_list.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
 
-const EmployeeList = () => {
+const PER_PAGE = 5;
+
+const EmployeeList = (props) => {
+    // props.employees => an array of employee objects
+
     return (
         <div>
             <div className="employee-list">
-                Employee List
+                {props.employees.map(employee => <EmployeeDetail key={employee._id}  employee={employee} />)}
             </div>
+            <button onClick={ () => { Meteor.subscribe('employees', 40) } } className="btn btn-primary">Load More...</button>
         </div>
     );
 };
 
-export default EmployeeList;
+export default createContainer(() => {
+    // set up subscription
+    Meteor.subscribe('employees', PER_PAGE);
+
+    // return an object. Whatever we return will be sent to EmployeeList
+    // as props
+    return { employees: Employees.find({}).fetch() };
+}, EmployeeList);
--- employees/server/main.js
+++ employees/server/main.js
@@ -6,7 +6,6 @@ import { image, helpers } from 'faker';
 
 Meteor.startup(() => {
     // generate data here
-
     // check to see if data exists in the collection
     const numberRecords = Employees.find({}).count();
     if (!numberRecords) {
@@ -15,11 +14,15 @@ Meteor.startup(() => {
             const { name, email, phone } = helpers.createCard();
 
             Employees.insert({
-                name,
-                email,
-                phone,
+                name, email, phone,
                 avatar: image.avatar()
             });
+
         });
     }
+
+    Meteor.publish('employees', function(per_page) {
+        // return a cursor
+        return Employees.find({}, { limit: per_page });
+    });
 });
