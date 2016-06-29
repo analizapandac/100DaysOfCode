61c74683a5d99547a767ed260c4b0c5a81c08e93
camperliz <analizapandac@gmail.com>
2016-06-29 09:24:19 +0800

    fix: change EmployeeList component to a class-based component

--- employees/client/components/employee_list.js
+++ employees/client/components/employee_list.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
 import { Employees } from '../../imports/collections/employees';
 import EmployeeDetail from './employee_detail';
 
-const PER_PAGE = 5;
+const PER_PAGE = 20;
 
-const EmployeeList = (props) => {
-    // props.employees => an array of employee objects
+class EmployeeList extends Component {
 
-    return (
-        <div>
-            <div className="employee-list">
-                {props.employees.map(employee => <EmployeeDetail key={employee._id}  employee={employee} />)}
+    componentWillMount() {
+        this.page = 1;
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div className="employee-list">
+                    { this.props.employees.map(employee => <EmployeeDetail key={employee._id}  employee={employee} />) }
+                </div>
+                <button onClick={ this.handleButtonClick.bind(this) } className="btn btn-primary">Load More...</button>
             </div>
-            <button onClick={ () => { Meteor.subscribe('employees', 40) } } className="btn btn-primary">Load More...</button>
-        </div>
-    );
+        );
+    };
+
+    handleButtonClick() {
+        Meteor.subscribe('employees', PER_PAGE * (this.page + 1));
+        this.page++;
+    }
+
 };
 
 export default createContainer(() => {
