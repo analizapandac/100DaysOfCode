7e6ce2141e84cf2a4bcbe554d709742e1185a9a5
analizapandac <analizapandac@gmail.com>
2016-07-25 13:49:59 +0800

    fix: finished angular2-tour-of-heroes

--- angular2/app/main.ts
+++ angular2/app/main.ts
@@ -1,4 +1,18 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
+// imports for loading & configuring the in-memory web api
+import { XHRBackend } from '@angular/http';
 
-bootstrap(AppComponent);
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { appRouterProviders } from './app.routes';
+
+bootstrap(AppComponent, [
+	appRouterProviders,
+	HTTP_PROVIDERS,
+	{ provide: XHRBackend, useClass: InMemoryBackendService }, //in-memory server
+	{ provide: SEED_DATA, useClass: InMemoryDataService } // in-memory server data
+]);
